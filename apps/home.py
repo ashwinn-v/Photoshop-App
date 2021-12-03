e2bf43ee400d013101fe0b37594dd527b1940bcb
Ashwin V <52974732+ashwinn-v@users.noreply.github.com>
2021-12-03 14:47:45 +0530

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -1,13 +1,12 @@
 import streamlit as st
-
+from PIL import Image
 def app():
-    st.title('19AIE303	Signal And Image Processing Project')
-
+    image = Image.open('imgs/lake.jpeg')
+    st.image(image, caption='Welcome to our webapp!', use_column_width=True)
+    st.subheader('19AIE303	Signal And Image Processing Project')
     st.subheader("Group 9", anchor=None)
-    st.header("Photoshop tool using Python and OpenCV", anchor=None)
+    st.subheader("Photoshop tool using Python and OpenCV", anchor=None)
     st.subheader("Team Members", anchor=None)
-
-
     st.markdown('Ashwin V   &emsp;&emsp; &emsp; &emsp;19018  <br> Devagopal AM &emsp; &emsp;19025  <br> Vishal Menon &nbsp;&nbsp;&emsp; &emsp;19070', unsafe_allow_html=True)
     st.subheader("Submitted to", anchor=None)
     st.markdown("Dr. Uma G")
